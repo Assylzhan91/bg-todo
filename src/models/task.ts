54e19d4053a8b9e2c92ac37e0777490065691c55
Assylzhan <asylzhan.baimuratov@gmail.com>
2024-01-30 22:56:59 +0600

    lesson-021

---
+++ src/models/task.ts
@@ -0,0 +1,15 @@
+import {Priority} from "./priority";
+import {Category} from "./category";
+
+export class Task {
+
+  constructor(
+    public id: number,
+    public title: string,
+    public completed: boolean,
+    public priority?: Priority,
+    public category?: Category,
+    public date?: Date
+  ) {
+  }
+}
