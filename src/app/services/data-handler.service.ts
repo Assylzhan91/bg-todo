743a794b039908462090860704d29cec0f0c08f2
Assylzhan <asylzhan.baimuratov@gmail.com>
2024-02-04 12:20:31 +0600

    lesson-026

--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@angular/core';
-import {Observable, of} from "rxjs";
+import {Injectable} from '@angular/core';
+import {delay, Observable, of} from "rxjs";
 
 import {CategoryType, TaskType, TestData} from '../data/TestData';
 
 @Injectable()
 export class DataHandlerService {
-  constructor() { }
+  constructor() {
+  }
 
   getCategories(): Observable<CategoryType[]> {
-    return of(TestData.categories)
+    return of(TestData.categories).pipe(delay(1000))
   }
 
   getTasks(): Observable<TaskType[]> {
     return of(TestData.tasks)
+  }
+
+  getTaskByCategory(category: CategoryType): TaskType[] {
+    return TestData.tasks.filter((task) => task.category === category)
   }
 }
