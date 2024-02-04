3920c17ce980636b22f005f145638ac872ef7571
Assylzhan <asylzhan.baimuratov@gmail.com>
2024-02-04 14:40:49 +0600

    lesson-031

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -3,11 +3,12 @@ import {CommonModule, NgFor} from '@angular/common';
 import {Observable} from "rxjs";
 import {TaskType} from "../../data/TestData";
 import {DataHandlerService} from "../../services/data-handler.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, NgFor],
+  imports: [CommonModule, NgFor, FormsModule],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
