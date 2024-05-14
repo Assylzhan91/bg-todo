05438f41f91cc2e559d0b0cac094c2175cffab6e
Асылжан Баймуратов <asylzanbajmuratov@Noutbuk-Asylzan.local>
2024-05-15 00:27:39 +0500

    lesson-056

--- src/app/data/dao/implements/TaskDAOArray.ts
+++ src/app/data/dao/implements/TaskDAOArray.ts
@@ -1,63 +1,64 @@
-import {TaskDAO} from "../interfaces/TaskDAO";
-import {Task} from "../../../models/task";
-import {EMPTY, Observable, of} from "rxjs";
-import {Category} from "../../../models/category";
-import {Priority} from "../../../models/priority";
-import {TaskType, TestData} from "../../TestData";
+import {EMPTY, Observable, of} from 'rxjs'
+
+import {Priority} from '../../../models/priority'
+import {Category} from '../../../models/category'
+import {TaskDAO} from '../interfaces/TaskDAO'
+import {Task} from '../../../models/task'
+import {TaskType, TestData} from '../../TestData'
 
 export class TaskDAOArray implements TaskDAO {
-
-  getAll(): Observable<TaskType[]>{
-    return of(TestData.tasks);
-  }
-
-  add(item: Task): Observable<Task> | unknown{
-    return undefined;
-  }
-
-  delete(id: number): Observable<Task[]>{
-    TestData.tasks = TestData.tasks.filter(task => task.id !== id)
+  getAll(): Observable<TaskType[]> {
     return of(TestData.tasks)
   }
 
-  get(id: number): Observable<Task> | unknown{
-    return undefined;
+  add(item: Task): Observable<Task> | unknown {
+    return undefined
   }
 
-
-  getCompletedCountInCategory(category: Category): Observable<number> | unknown{
-    return undefined;
+  delete(id: number): Observable<Task[]> {
+    TestData.tasks = TestData.tasks.filter((task) => task.id !== id)
+    return of(TestData.tasks)
   }
 
-  getTotalCount(category: Category): Observable<number> | unknown{
-    return undefined;
+  get(id: number): Observable<Task> | unknown {
+    return undefined
   }
 
-  getTotalCountInCategory(category: Category): Observable<number> | unknown{
-    return undefined;
+  getCompletedCountInCategory(category: Category): Observable<number> | unknown {
+    return undefined
   }
 
-  getUncompletedCountInCategory(category: Category): Observable<number> | unknown{
-    return undefined;
+  getTotalCount(category: Category): Observable<number> | unknown {
+    return undefined
   }
 
-  search(category: Category, searchText: string | null, status: boolean | null, priority: Priority | null): Observable<Task[]>{
+  getTotalCountInCategory(category: Category): Observable<number> | unknown {
+    return undefined
+  }
+
+  getUncompletedCountInCategory(category: Category): Observable<number> | unknown {
+    return undefined
+  }
+
+  search(
+    category: Category,
+    searchText: string | null,
+    status: boolean | null,
+    priority: Priority | null,
+  ): Observable<Task[]> {
     return of(
       category.id === 0
         ? TestData.tasks
-        : TestData.tasks.filter(task=> task.category?.id == category.id)
+        : TestData.tasks.filter((task) => task.category?.id == category.id),
     )
   }
 
-  update(task: Task): Observable<Task>{
-    const taskTmp = TestData.tasks.find(t => t.id == task.id)
-    if (taskTmp){
+  update(task: Task): Observable<Task> {
+    const taskTmp = TestData.tasks.find((t) => t.id == task.id)
+    if (taskTmp) {
       TestData.tasks.splice(TestData.tasks.indexOf(taskTmp), 1, task)
-      return of(task);
+      return of(task)
     }
     return EMPTY
-
   }
-
-
 }
--- src/app/services/data-handler.service.ts
+++ src/app/services/data-handler.service.ts
@@ -1,36 +1,35 @@
-import {inject, Injectable} from '@angular/core';
-import {delay, Observable} from "rxjs";
+import {inject, Injectable} from '@angular/core'
+import {delay, Observable} from 'rxjs'
 
-import {CategoryType, TaskType} from '../data/TestData';
-import {TaskDAOArray} from "../data/dao/implements/TaskDAOArray";
-import {CategoryDAOArray} from "../data/dao/implements/CategoryDAOArray";
-import {Priority} from "../models/priority";
-import {Task} from "../models/task";
+import {CategoryType, TaskType} from '../data/TestData'
+import {TaskDAOArray} from '../data/dao/implements/TaskDAOArray'
+import {CategoryDAOArray} from '../data/dao/implements/CategoryDAOArray'
+import {Priority} from '../models/priority'
+import {Task} from '../models/task'
 
 @Injectable()
 export class DataHandlerService {
-  taskDAOArray =  inject(TaskDAOArray)
-  categoryDAOArray =  inject(CategoryDAOArray)
+  taskDAOArray = inject(TaskDAOArray)
+  categoryDAOArray = inject(CategoryDAOArray)
 
-  getAllCategories$ = this.categoryDAOArray.getAllCategory().pipe(delay(10))
-  getAllPriorities$ = this.categoryDAOArray.getAllPriorities().pipe(delay(30))
-  getAllTasks$ = this.taskDAOArray.getAll().pipe(delay(300))
+  getAllCategories$ = this.categoryDAOArray.getAllCategory().pipe(delay(1000))
+  getAllPriorities$ = this.categoryDAOArray.getAllPriorities().pipe(delay(3000))
+  getAllTasks$ = this.taskDAOArray.getAll().pipe(delay(2000))
 
   searchTasks(
     searchTasks: CategoryType,
     searchText: string | null,
     status: boolean | null,
     priority: Priority | null,
-  ): Observable<TaskType[]>{
+  ): Observable<TaskType[]> {
     return this.taskDAOArray.search(searchTasks, searchText, status, priority)
   }
 
-  updateTask(task: Task): Observable<Task>{
+  updateTask(task: Task): Observable<Task> {
     return this.taskDAOArray.update(task)
   }
 
-  deleteTask(id: number): Observable<Task[]>{
+  deleteTask(id: number): Observable<Task[]> {
     return this.taskDAOArray.delete(id)
   }
-
 }
